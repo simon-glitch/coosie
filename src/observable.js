b3394b78bdacbcdc417ad01b16abc3688334ce50
simon-glitch <simontalksalot@gmail.com>
2026-07-15 13:10:38 -0400

    add simple test

--- src/observable.js
+++ src/observable.js
@@ -1317,6 +1317,18 @@ class App{
     }
 }
 
-// testing time!
+const app = new App(100);
 
+// testing time!
+const content = new Content({
+    app,
+    query: ".content",
+    content: {
+        value: "Starting value.",
+        calculate: function(){
+            return app.curr_t;
+        },
+        publishers: app.o_now,
+    },
+});
 
