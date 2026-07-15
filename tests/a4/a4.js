ad43a80b0445e7d707da393649d9c8dff6737154
simon-glitch <simontalksalot@gmail.com>
2026-07-15 15:22:15 -0400

    fix issues with next and other issues

--- tests/a4/a4.js
+++ tests/a4/a4.js
@@ -8,11 +8,48 @@ const content = new Content({
         name: "my_value",
         value: "Starting value.",
         calculate: function(){
-            console.log("It works!");
             return app.curr_t;
         },
         publishers: app.o_now,
     },
+    observables: [
+        {name: "cookies",},
+        {name: "cookies_t", publishers: "cookies", calculate: function(){
+            return Math.floor(this.cookies);
+        }},
+        {name: "dollars", publishers: "cookies", calculate: function(){
+            return this.cookies * 2;
+        }},
+        {name: "dollars_t", publishers: "dollars", calculate: function(){
+            return
+            Math.floor(this.dollars) + "." +
+            Math.floor((10 * this.dollars) % 10) +
+            Math.floor((100 * this.dollars) % 10);
+        }},
+        {name: "grandmas",},
+        {name: "cps_mul",},
+        {name: "cps_on", publishers: "cps_mul", calculate: function(){
+            return ["off", "on"][this.cps_mul];
+        }},
+        {name: "cps", publishers: ["grandmas", "cps_mul"], calculate: function(){
+            return this.grandmas * this.cps_mul;
+        }},
+        {name: "time", next: {
+            calculate: function(){
+                this.time + 1 * 0.001
+            },
+            publishers: app.dt,
+        },},
+        {name: "seconds",},
+        {name: "seconds_t",},
+        {name: "time_m",},
+        {name: "minutes",},
+        {name: "hours",},
+        {name: "o_b_gc",},
+        {name: "o_b_gg",},
+        {name: "o_b_t_cps",},
+    ],
 });
+
 
 app.start();
